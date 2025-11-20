2650eb21541935eb7171a2097a67adfc2111e4c7
rohitlandage86 <rohitlandage86@gmail.com>
2025-11-20 15:42:11 +0530

    feature/helpdesk

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    baseUrl: 'http://crm.tecstaq.in:3001/',
+    baseUrl: 'https://support.tecstaq.com:3000/',
     // baseUrl: 'http://localhost:3000/',
 };  
